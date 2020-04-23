930a35a9411db587b1d7e05aa9e8f4015547691b
RA404 <61162669+RA404@users.noreply.github.com>
2020-04-23 12:06:08 +0500

    Pull request from develop to master (#1)
    * users completed
    
    * added readme, cards routes and 404
    
    * fixed set header error
    
    * fixed some errors and a bit refactored

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+
 const router = require('./routes');
 
 const { PORT = 3000 } = process.env;
@@ -9,9 +10,15 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', router);
 
+
+// Handling 404
+app.use((req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
+
 app.listen(PORT, () => {
-  //    console.log(`App listening ${PORT}`);
+  // eslint-disable-next-line no-console
+  console.log(`App listening ${PORT}`);
 });
